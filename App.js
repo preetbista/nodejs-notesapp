f31264da09a5efcf6d98a3089bbd42106cf89502
Preet Bista <bistapreet4@gmail.com>
2023-02-23 13:43:23 +0545

    used yargs

--- App.js
+++ App.js
@@ -1,8 +1,60 @@
 const chalk = require('chalk')
+const yargs = require('yargs')
 const getNote = require('./Notes.js')
 
-const disp = getNote()
-console.log(disp)
+//Customize yargs version
+yargs.version('1.1.0')
 
-const blueMsg = chalk.blue('Hello') + chalk.green('world') + chalk.red('!');
-console.log(blueMsg);
+//Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note Title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: (argv) => {
+        console.log('Title: ' + argv.title)
+        console.log('Body: ' + argv.body)
+    }
+})
+
+//create remove command 
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note!',
+    handler: () => {
+        console.log('Removing the note!')
+    }
+})
+
+//create list command 
+yargs.command({
+    command: 'list',
+    describe: 'List your note',
+    handler: () => {
+        console.log('Listing out all notes!')
+    }
+})
+
+//create read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a list',
+    handler: () => {
+        console.log('Reading the list!')
+    }
+})
+
+yargs.parse()
+
+// console.log(yargs.argv)
+
